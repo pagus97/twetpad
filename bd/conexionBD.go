3b0fc333dfdb9fcbfd41116e846fa51878a184c6
pagus97 <pablo290997@gmail.com>
2023-03-20 11:28:37 -0300

    Registro de usuario

---
+++ bd/conexionBD.go
@@ -0,0 +1,38 @@
+package bd
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+/*MongoCN es el objeto de conexion a la BD*/
+var MongoCN = ConectarBD()
+var clientOptions = options.Client().ApplyURI("mongodb+srv://pablo290997:Santi2910.@twetpad.ien5e96.mongodb.net/test")
+
+/*Conectar BD es la funcion que me permite conectar la BD */
+func ConectarBD() *mongo.Client {
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err.Error())
+		return client
+	}
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err.Error())
+		return client
+	}
+	log.Println("Conexion exitosa con la BD")
+	return client
+}
+
+/*ChequeoConnection es el Ping a la BD*/
+func ChequeoConnection() int {
+	err := MongoCN.Ping(context.TODO(), nil)
+	if err != nil {
+		return 0
+	}
+	return 1
+}
--- go.mod
+++ go.mod
@@ -3,5 +3,10 @@ module github.com/pagus97/twetpad
 go 1.13
 
 require (
-
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/gomodule/redigo v1.8.9 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/rs/cors v1.8.3 // indirect
+	go.mongodb.org/mongo-driver v1.11.3 // indirect
+	golang.org/x/crypto v0.7.0 // indirect
 )
