8205f871fd1cd38acaa26c8bacf02e9c339d0077
pagus97 <pablo290997@gmail.com>
2023-03-22 09:48:07 -0300

    leotweets

---
+++ routers/leoTweets.go
@@ -0,0 +1,40 @@
+package routers
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/pagus97/twetpad/bd"
+)
+
+/*LeoTweets leo los Tweets*/
+func LeoTweets(w http.ResponseWriter, r *http.Request) {
+
+	ID := r.URL.Query().Get("id")
+	if len(ID) < 1 {
+		http.Error(w, "Debe enviar el parametro id", http.StatusBadRequest)
+		return
+	}
+
+	if len(r.URL.Query().Get("pagina")) < 1 {
+		http.Error(w, "Debe enviar el parametro pagina", http.StatusBadRequest)
+		return
+	}
+	pagina, err := strconv.Atoi(r.URL.Query().Get("pagina"))
+	if err != nil {
+		http.Error(w, "Debe enviar el parametro pagina con un valor mayor a 0", http.StatusBadRequest)
+		return
+	}
+
+	pag := int64(pagina)
+	respuesta, correcto := bd.LeoTweets(ID, pag)
+	if correcto == false {
+		http.Error(w, "Error al leer los tweets", http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(respuesta)
+}
